f43df907229086f5eeb0d99d45811b1ed530e57c
maria.nunez <maria.nunez@wefox.com>
2022-01-07 16:07:06 +0100

    recycler view

--- app/src/main/java/com/marianunez/todoapp/adapter/ToDoAdapter.kt
+++ app/src/main/java/com/marianunez/todoapp/adapter/ToDoAdapter.kt
@@ -4,21 +4,26 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.marianunez.todoapp.R
+import com.marianunez.todoapp.TaskList
 
-class ToDoAdapter: RecyclerView.Adapter<ToDoViewHolder>() {
-
-    private val toDoList = mutableListOf("Android Development", "House Work", "Errands", "Shopping")
+class ToDoAdapter(private val toDoList: ArrayList<TaskList>) : RecyclerView.Adapter<ToDoViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ToDoViewHolder {
-        val adapterLayout = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
+        val adapterLayout =
+            LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
         return ToDoViewHolder((adapterLayout))
     }
 
     override fun onBindViewHolder(holder: ToDoViewHolder, position: Int) {
         holder.itemNumber.text = (position + 1).toString()
-        holder.itemTitle.text = toDoList[position]
+        holder.itemTitle.text = toDoList[position].title
     }
 
     override fun getItemCount(): Int = toDoList.size
+
+    fun addNewItem(list: TaskList) {
+        toDoList.add(list)
+        notifyItemInserted(toDoList.size - 1)
+    }
 
 }
--- app/src/main/java/com/marianunez/todoapp/adapter/ToDoViewHolder.kt
+++ app/src/main/java/com/marianunez/todoapp/adapter/ToDoViewHolder.kt
@@ -4,14 +4,12 @@ import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.marianunez.todoapp.databinding.ListItemBinding
 
-class ToDoViewHolder(view: View): RecyclerView.ViewHolder(view) {
+class ToDoViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
     private val binding = ListItemBinding.bind(view)
 
     val itemTitle = binding.itemTitle
-    val itemDescription = binding.itemDescription
     val itemNumber = binding.itemNumber
-
 
 
 }
