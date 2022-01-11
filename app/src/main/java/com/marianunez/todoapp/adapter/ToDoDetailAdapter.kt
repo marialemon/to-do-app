63848754143a20396b94ae785f054088dee461e8
maria.nunez <maria.nunez@wefox.com>
2022-01-11 17:27:33 +0100

    add detail activity and other recycler view

---
+++ app/src/main/java/com/marianunez/todoapp/adapter/ToDoDetailAdapter.kt
@@ -0,0 +1,26 @@
+package com.marianunez.todoapp.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.marianunez.todoapp.R
+import com.marianunez.todoapp.TaskList
+
+
+class ToDoDetailAdapter(private val toDoDetailList: TaskList) :
+    RecyclerView.Adapter<ToDoDetailViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ToDoDetailViewHolder {
+        val adapterLayout =
+            LayoutInflater.from(parent.context).inflate(R.layout.detail_list_item, parent, false)
+        return ToDoDetailViewHolder((adapterLayout))
+    }
+
+    override fun onBindViewHolder(holder: ToDoDetailViewHolder, position: Int) {
+        holder.itemCheckbox.text = toDoDetailList.taskList[position]
+    }
+
+    override fun getItemCount(): Int = toDoDetailList.taskList.size
+
+}
+
---
+++ app/src/main/java/com/marianunez/todoapp/adapter/ToDoDetailViewHolder.kt
@@ -0,0 +1,12 @@
+package com.marianunez.todoapp.adapter
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.marianunez.todoapp.databinding.DetailListItemBinding
+
+class ToDoDetailViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    private val binding = DetailListItemBinding.bind(view)
+
+    val itemCheckbox = binding.checkbox
+}
